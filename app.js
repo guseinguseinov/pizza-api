640a78baa054988b4998a5068121a85062fe78aa
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-08-03 15:31:27 +0100

    admin moved

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 
-import adminRoute from "./routes/admin/adminRoute.js";
-import adminProductRoute from "./routes/admin/productAdminRoute.js";
  
 config();
 await mongoose.connect(process.env.DB_CONNECTION_STRING);
 
 const app = express();
-const admin = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,11 +19,6 @@ app.use('/users', userRoute);
 app.use('/products', productRoute);
 app.use('/cart', cartRoute);
 
-// admin
-admin.use(express.urlencoded({ extended: true }));
-admin.use(express.json());
 
-admin.use('/', adminRoute);
-admin.use('/products', adminProductRoute);
 
-export {app , admin};
+export default app;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 import http from 'http';
-import { app, admin } from './app.js';
+import app from './app.js';
 
 const server = http.createServer(app);
 const adminServer = http.createServer(admin);
 
 server.listen(8080, () => {
     console.log('Server listen to http://localhost:8080');
-});
-
-adminServer.listen(8083, () => {
-    console.log('Admin listen to http://localhost:8083');
 });
